3869dac3277eb2c880c7dc1604f74af4e5ef9006
heefCodes <abdulkareemtoyin277@gmail.com>
2022-12-14 11:17:41 +0100

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operato,r> <b>")
+        print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
 
     operator = argv[2]
