05665b6d403caafc4b8bcf447ece61ebffcbd0ca
heefCodes <abdulkareemtoyin277@gmail.com>
2022-12-20 14:53:59 +0100

    size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module 2-square
+Define class Square
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Arg:
+        size: size of a square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes Square
+
+        Attribute:
+            size: size of a square, default to 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
