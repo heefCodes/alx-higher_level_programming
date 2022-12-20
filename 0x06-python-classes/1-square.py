6ee11171c9d8be75803a2a8b3e727460fdfe8e0d
heefCodes <abdulkareemtoyin277@gmail.com>
2022-12-20 14:04:24 +0100

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square
+
+"""
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size: size of a square
+    """
+    def __init__(self, size):
+        """
+        Initializes square
+
+        Attribute:
+            size: size of a square
+        """
+        self.__size  = size
