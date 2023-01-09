198f9d05c61a01e21da4ac5e7d8cf2a5731ceae1
heefCodes <abdulkareemtoyin277@gmail.com>
2023-01-09 21:05:18 +0100

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry definition
+    """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
