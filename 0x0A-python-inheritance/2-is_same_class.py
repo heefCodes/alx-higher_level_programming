19062f01fbe8decd5fbf697f5a242d1ece5bd8f7
heefCodes <abdulkareemtoyin277@gmail.com>
2023-01-09 16:13:36 +0100

    get instances

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if an object is an instance of a specified class
+    """
+    return type(obj) == a_class
+
