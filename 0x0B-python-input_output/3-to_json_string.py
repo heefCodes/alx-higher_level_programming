c9de27f63058f3224d6c25a863d4381a5eeb3440
heefCodes <abdulkareemtoyin277@gmail.com>
2023-01-12 13:34:09 +0100

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 3-to_json_string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """_summary_
+
+    Args:
+        my_obj (string_): _description_
+
+    Returns:
+        _JSON representation
+    """
+    return json.dumps(my_obj)
